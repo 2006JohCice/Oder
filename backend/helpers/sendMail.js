68f5b3a4bb12dd0e16e1cf899d776b87ec7a8e0d
titvvn <nhatluong0987.email@gmail.com>
2026-04-15 02:48:34 +0700

    update and use resend

--- backend/helpers/sendMail.js
+++ backend/helpers/sendMail.js
@@ -28,30 +28,50 @@ const nodemailer = require("nodemailer");
 
 
 
+// module.exports.sendMail = async (email, subject, html) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: process.env.EMAIL_USER,
+//       to: email,
+//       subject: subject,
+//       html: html,
+//     };
+
+//     const info = await transporter.sendMail(mailOptions);
+
+//     console.log("✅ Email sent:", info.response);
+//     return true;
+
+//   } catch (error) {
+//     console.log("Lỗi gửi mail:", error);
+//     throw error;
+//   }
+// };
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 module.exports.sendMail = async (email, subject, html) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
+    await resend.emails.send({
       from: process.env.EMAIL_USER,
       to: email,
       subject: subject,
       html: html,
-    };
+    });
 
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log("✅ Email sent:", info.response);
-    return true;
-
+    console.log(" gửi mail OK");
   } catch (error) {
-    console.log("Lỗi gửi mail:", error);
+    console.log(" lỗi gửi mail:", error);
     throw error;
   }
 };
